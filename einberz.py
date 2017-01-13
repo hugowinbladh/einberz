4572a87dcbc879184674db789de6e8021df63b5e
Hugo Winbladh <hugowinbladh@gmail.com>
2017-01-13 08:31:13 +0100

    bug fixes

--- einberz.py
+++ einberz.py
@@ -1,19 +1,24 @@
 from PIL import Image
 from PIL import ImageOps
+import sys
 def einberz(image):
-    bg = Image.new('RGB', (1280, 2272), (255,255,255))
     img = Image.open(image)
-    img = ImageOps.fit(img,(40,71),0,0,(0,0))
+    img_w, img_h = img.size
+    bg = Image.new('RGB', (img_w, img_h), (255,255,255))
+    img = ImageOps.fit(img,(img_w / 32, img_h / 32),0,0,(0,0))
+    img = ImageOps.grayscale(img)
+    print(img_w)
+    print(img_w/32)
     pix = img.load()
-    for i in range(0,71):
-        for j in range(0,40):
+    for i in range(0,img_h/32):
+        for j in range(0,img_w/32):
             berz = Image.open("berzan-logga.jpg")
-            value = list(pix[j,i])
-            size = (int(-value[0]/8+45),int(-value[0]/8+45))
+            value = pix[j,i]
+            size = (int(-value/8)+45,int(-value/8)+45)
             border = -list(size)[0]/32
-            berz = ImageOps.grayscale(berz)
             berz = ImageOps.fit(berz, size, 0, 0, (0.0, 0.0))
             bg.paste(berz,(j*32,i*32))
     bg.save("out.jpg")
-    img.save("out2.jpg")
-einberz("wow.jpg")
+print "enter your image's filename:"
+filename = raw_input()
+einberz(filename)
